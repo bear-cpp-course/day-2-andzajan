b3b9bf6cc8e722fcd932d24ad402cc427b48a871
LHurst-UoB <44084717+LHurst-UoB@users.noreply.github.com>
2020-02-14 10:44:11 +0000

    Initial commit

---
+++ mpags-cipher.cpp
@@ -0,0 +1,170 @@
+// Standard Library includes
+#include <iostream>
+#include <string>
+#include <vector>
+
+// For std::isalpha and std::isupper
+#include <cctype>
+
+// Main function of the mpags-cipher program
+int main(int argc, char* argv[])
+{
+  // Convert the command-line arguments into a more easily usable form
+  const std::vector<std::string> cmdLineArgs {argv, argv+argc};
+
+  // Add a typedef that assigns another name for the given type for clarity
+  typedef std::vector<std::string>::size_type size_type;
+  const size_type nCmdLineArgs {cmdLineArgs.size()};
+
+  // Options that might be set by the command-line arguments
+  bool helpRequested {false};
+  bool versionRequested {false};
+  std::string inputFile {""};
+  std::string outputFile {""};
+
+  // Process command line arguments - ignore zeroth element, as we know this to
+  // be the program name and don't need to worry about it
+  for (size_type i {1}; i < nCmdLineArgs; ++i) {
+
+    if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
+      helpRequested = true;
+    }
+    else if (cmdLineArgs[i] == "--version") {
+      versionRequested = true;
+    }
+    else if (cmdLineArgs[i] == "-i") {
+      // Handle input file option
+      // Next element is filename unless -i is the last argument
+      if (i == nCmdLineArgs-1) {
+	std::cerr << "[error] -i requires a filename argument" << std::endl;
+	// exit main with non-zero return to indicate failure
+	return 1;
+      }
+      else {
+	// Got filename, so assign value and advance past it
+	inputFile = cmdLineArgs[i+1];
+	++i;
+      }
+    }
+    else if (cmdLineArgs[i] == "-o") {
+      // Handle output file option
+      // Next element is filename unless -o is the last argument
+      if (i == nCmdLineArgs-1) {
+	std::cerr << "[error] -o requires a filename argument" << std::endl;
+	// exit main with non-zero return to indicate failure
+	return 1;
+      }
+      else {
+	// Got filename, so assign value and advance past it
+	outputFile = cmdLineArgs[i+1];
+	++i;
+      }
+    }
+    else {
+      // Have an unknown flag to output error message and return non-zero
+      // exit status to indicate failure
+      std::cerr << "[error] unknown argument '" << cmdLineArgs[i] << "'\n";
+      return 1;
+    }
+  }
+
+  // Handle help, if requested
+  if (helpRequested) {
+    // Line splitting for readability
+    std::cout
+      << "Usage: mpags-cipher [-i <file>] [-o <file>]\n\n"
+      << "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
+      << "Available options:\n\n"
+      << "  -h|--help        Print this help message and exit\n\n"
+      << "  --version        Print version information\n\n"
+      << "  -i FILE          Read text to be processed from FILE\n"
+      << "                   Stdin will be used if not supplied\n\n"
+      << "  -o FILE          Write processed text to FILE\n"
+      << "                   Stdout will be used if not supplied\n\n";
+    // Help requires no further action, so return from main
+    // with 0 used to indicate success
+    return 0;
+  }
+
+  // Handle version, if requested
+  // Like help, requires no further action,
+  // so return from main with zero to indicate success
+  if (versionRequested) {
+    std::cout << "0.1.0" << std::endl;
+    return 0;
+  }
+
+  // Initialise variables for processing input text
+  char inputChar {'x'};
+  std::string inputText {""};
+
+  // Read in user input from stdin/file
+  // Warn that input file option not yet implemented
+  if (!inputFile.empty()) {
+    std::cout << "[warning] input from file ('"
+              << inputFile
+              << "') not implemented yet, using stdin\n";
+  }
+
+  // Loop over each character from user input
+  // (until Return then CTRL-D (EOF) pressed)
+  while(std::cin >> inputChar)
+  {
+    // Uppercase alphabetic characters
+    if (std::isalpha(inputChar)) {
+      inputText += std::toupper(inputChar);
+      continue;
+    }
+
+    // Transliterate digits to English words
+    switch (inputChar) {
+      case '0':
+	inputText += "ZERO";
+	break;
+      case '1':
+	inputText += "ONE";
+	break;
+      case '2':
+	inputText += "TWO";
+	break;
+      case '3':
+	inputText += "THREE";
+	break;
+      case '4':
+	inputText += "FOUR";
+	break;
+      case '5':
+	inputText += "FIVE";
+	break;
+      case '6':
+	inputText += "SIX";
+	break;
+      case '7':
+	inputText += "SEVEN";
+	break;
+      case '8':
+	inputText += "EIGHT";
+	break;
+      case '9':
+	inputText += "NINE";
+	break;
+    }
+
+    // If the character isn't alphabetic or numeric, DONT add it.
+    // Our ciphers can only operate on alphabetic characters.
+  }
+
+  // Output the transliterated text
+  // Warn that output file option not yet implemented
+  if (!outputFile.empty()) {
+    std::cout << "[warning] output to file ('"
+              << outputFile
+              << "') not implemented yet, using stdout\n";
+  }
+
+  std::cout << inputText << std::endl;
+
+  // No requirement to return from main, but we do so for clarity
+  // and for consistency with other functions
+  return 0;
+}
